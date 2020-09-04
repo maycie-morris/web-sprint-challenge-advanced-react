3460c50e70c2977ae18f9805d9249b8b4927f961
Maycie Morris <maycie.morris@gmail.com>
2020-09-04 13:39:24 -0400

    First test complete, most of second

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,33 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    screen.getByText(/checkout form/i)
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+
+    const firstNameInput = screen.getByLabelText(/First Name:/i)
+    const lastNameInput = screen.getByLabelText(/Last Name:/i)
+    const addressInput = screen.getByLabelText(/Address:/i)
+    const cityInput = screen.getByLabelText(/City:/i)
+    const stateInput = screen.getByLabelText(/State:/i)
+    const zipInput = screen.getByLabelText(/Zip:/i)
+
+    fireEvent.change(firstNameInput, {target: {value: "Maycie"} })
+    fireEvent.change(lastNameInput, {target: {value: "Morris"} } )
+    fireEvent.change(addressInput, {target: {value: "123 Address St"} } )
+    fireEvent.change(cityInput {target: {value: "City"} } )
+    fireEvent.change(stateInput, {target: {value: "State"} } )
+    fireEvent.change(zipInput, {target: {value: "12345"} })
+
+    const submitButton = screen.getByText(/CHECKOUT/i)
+    fireEvent.click(submitButton)
+
+});
